f3d1be4dec7f6de4a393d8c85ffaf9e0c4209afc
nslmanu <nslmanu@me.com>
2026-07-31 15:11:08 +0200

    first commit

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -7,15 +7,15 @@
  *
  * Return: pointer to dest
  */
-char *_strcpy(char *dest, char *src)   // fonction qui reçoit destination et source, renverra dest
+char *_strcpy(char *dest, char *src)
 {
-	int i = 0;                     // index de parcours, démarre à la case 0
+	int i = 0;
 
-	while (src[i] != '\0')         // tant qu'on n'a pas atteint la fin de la source
+	while (src[i] != '\0')
 	{
-		dest[i] = src[i];      // copie le caractère courant de src vers dest
-		i++;                   // passe à la case suivante
+		dest[i] = src[i];
+		i++;
 	}
-	dest[i] = '\0';                // ajoute le marqueur de fin à la copie
-	return (dest);                 // renvoie le pointeur vers la destination
+	dest[i] = '\0';
+	return (dest);
 }
