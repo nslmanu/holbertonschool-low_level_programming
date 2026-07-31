a5554c97890bf95303a8aee7d1211ff3b89f188e
nslmanu <nslmanu@me.com>
2026-07-31 15:08:18 +0200

    first commit

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**
+ * _strcpy - copies a string including the null byte to dest
+ * @dest: pointer to the destination buffer
+ * @src: pointer to the source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)   // fonction qui reçoit destination et source, renverra dest
+{
+	int i = 0;                     // index de parcours, démarre à la case 0
+
+	while (src[i] != '\0')         // tant qu'on n'a pas atteint la fin de la source
+	{
+		dest[i] = src[i];      // copie le caractère courant de src vers dest
+		i++;                   // passe à la case suivante
+	}
+	dest[i] = '\0';                // ajoute le marqueur de fin à la copie
+	return (dest);                 // renvoie le pointeur vers la destination
+}
