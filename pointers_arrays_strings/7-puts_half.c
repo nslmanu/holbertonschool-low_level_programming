2396d7d24c72149743613314a6d30f37d07c8636
nslmanu <nslmanu@me.com>
2026-07-31 12:20:41 +0200

    first commit

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+/**
+ * puts_half - prints the second half of a string
+ * @str: pointer to the string to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	i = len / 2;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
