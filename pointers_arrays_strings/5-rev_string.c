74a2738ad109d14c9ebeb981c4914255d0210c5a
nslmanu <nslmanu@me.com>
2026-07-31 12:03:31 +0200

    first commit

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,28 @@
+#include "main.h"
 
+/**
+ * rev_string - reverses a string in place
+ * @s: pointer to the string to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i = 0;
+	char tmp;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	len--;
+	while (i < len)
+	{
+		tmp = s[i];
+		s[i] = s[len];
+		s[len] = tmp;
+		i++;
+		len--;
+	}
+}
