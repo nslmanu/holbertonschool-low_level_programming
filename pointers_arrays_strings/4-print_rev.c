93628d973df4297eeeeceed0d68a5f72d9f5cc42
nslmanu <nslmanu@me.com>
2026-07-31 11:55:07 +0200

    first commit

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,2 +1,23 @@
+#include "main.h"
 
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: pointer to the string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = 0;
 
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	while (len > 0)
+	{
+		len--;
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
